42b405fbbe299f8cd6d367355401b43e9a1f8ed2
Dominique Makowski <dom.mak19@gmail.com>
2021-02-08 13:33:07 +0800

    Move components to JSmisc

--- scripts/commit_to_github.js
+++
@@ -1,7 +0,0 @@
-// const gh = new GitHub({ token: '66078b465a517e60d86050a787195b23c381a33d' }) // 4ce322bfb34df364549f' })
-
-// const user = gh.getUser('DominiqueMakowski')
-
-// user.listStarredRepos(function (err, repos) {
-//     console.log(repos)
-// })
