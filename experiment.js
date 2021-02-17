fa3e240e0b47beb3876fa264ac48f0e7c74e5757
Amelia Huang <amelia.huang@skyscanner.net>
2021-02-17 09:37:31 +0800

    test json

--- experiment.js
+++ experiment.js
@@ -25,7 +25,7 @@ octokit.repos.createOrUpdateFileContents({
         repo: REPO_NAME,
         path: `${test}.json`, // path in repo -- saves to 'results' folder as '<participant_id>.json'
         message: `Saving results for participant ${test}`, // commit message
-        content: "fart", // octokit requires base64 encoding for the content; this just encodes the json string
+        content: {fart: 'fart'}, // octokit requires base64 encoding for the content; this just encodes the json string
         "committer.name": REPO_OWNER,
         "committer.email": AUTHOR_EMAIL,
         "author.name": REPO_OWNER,
