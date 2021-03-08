89bd395a465c61c69f671d920f94e46da87993f7
Tam Pham <phamttam17@gmail.com>
2021-03-09 00:17:21 +0800

    try netlify

--- .netlify/functions/api.js
+++ .netlify/functions/api.js
@@ -1,8 +1,17 @@
+//upload data
+//authenticate with octokit
+
+import { Octokit } from "https://cdn.skypack.dev/@octokit/rest";
+
 exports.handler = async (event, context) => {
+    const octokit = new Octokit({
+        auth: process.env.GH_OAUTH, // authenticating Octokit
+    })
+    const { data } = await octokit.request("/user");
     return {
         statusCode: 200,
         body: JSON.stringify({
-            auth: process.env.GH_OAUTH
+            result: "ok"
         }),
     }
 }
